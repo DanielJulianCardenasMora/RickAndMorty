c9c5c876fce7f391e945a071396607d3529c44f0
danieljulian <danielcamora@hotmail.com>
2024-01-31 21:10:43 -0500

    Testing supertest

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -10,15 +10,10 @@ const URL = "https://rickandmortyapi.com/api/character/";
 const getCharById = async (req, res) => {
 
   try {
-    const charId = req.params.id;
-    // req.params = { id: "33" }
-    
-    const { data } = await axios.get(`${URL}${charId}`);
-    // { data: {...} }
-
+    const charId = req.params.id; // req.params = { id: "33" }
+    const { data } = await axios.get(`${URL}${charId}`); // { data: {...} }
     const { id, status, name, species, origin, image, gender } = data;
     const character = { id, status, name, species, origin, image, gender };
-
     return character.name
       ? res.json(character)
       : res.status(404).send("Not found.")
--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,4 @@
-const express = require("express");
-const router = require("./routes");
-const server = express();
+const server = require('./app');
 const PORT = 3001;
 
 
@@ -8,25 +6,6 @@ const PORT = 3001;
 
 
 
-
-// Middlewares
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header(
-     'Access-Control-Allow-Headers',
-     'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  res.header(
-     'Access-Control-Allow-Methods',
-     'GET, POST, OPTIONS, PUT, DELETE'
-  );
-  next();
-});
-server.use(express.json())
-server.use('/rickandmorty', router)
-
-// Rutas
 
 
 server.listen(PORT, () => {
