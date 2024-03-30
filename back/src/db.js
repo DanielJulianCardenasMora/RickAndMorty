b91d8a7e7dce9af2a92d3d5deb9211b8f587e432
danieljulian <danielcamora@hotmail.com>
2024-03-30 17:53:51 -0500

    Deploying

--- back/src/db.js
+++ back/src/db.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URL } = process.env;
 const userModelFn = require('./models/User')
 const favoriteModelFn = require('./models/Favorite')
 
 
 const sequelize = new Sequelize(
-   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+   DB_URL,
    { logging: false, native: false }
 );
 
@@ -14,18 +15,12 @@ const sequelize = new Sequelize(
 userModelFn(sequelize)
 favoriteModelFn(sequelize)
 
-//  { User:User, Favorite:Favorite }
 
-// Ejercicio 06
-// ¡Relaciona tus modelos aquí abajo!
 const { User, Favorite } = sequelize.models;
 
 
 User.belongsToMany(Favorite, { through: 'user_favorite' })
-
 Favorite.belongsToMany(User, { through: 'user_favorite' } )
-
-
 
 
 
