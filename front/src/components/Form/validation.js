2ddd181aabd1550ba6b08fca88fc2da031a8660b
danieljulian <danielcamora@hotmail.com>
2024-04-17 20:20:04 -0500

    x

--- front/src/components/Form/validation.js
+++ front/src/components/Form/validation.js
@@ -12,7 +12,7 @@ const regexPassword = /^(?=.*\d)[0-9a-zA-Z]{6,10}$/
 
 function validation(data) {
     const errors = {}
-    if (!regexEmail.test(data.email)) errors.email = 'Debe ser un email'
+    // if (!regexEmail.test(data.email)) errors.email = 'Debe ser un email'
     if (!expresionRegular.test(data.email)) errors.email = 'Should have 1 to 35 characters'
     if (!regexPassword.test(data.password)) errors.password = 'Should have at least one number'
     return errors
